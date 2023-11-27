a5a4aa338b1ba7c3aaa6c809e291845ae0cc3403
JoseEduardoSS <joseesouza14@gmail.com>
2023-11-27 20:48:06 -0300

    atualizando projeto

--- src/main/java/br/com/estruturaDeDados/Utils.java
+++ src/main/java/br/com/estruturaDeDados/Utils.java
@@ -1,7 +1,5 @@
 package br.com.estruturaDeDados;
 
-import java.util.List;
-
 public class Utils {
 
     public static String capitalizeWords(String str) {
@@ -20,34 +18,6 @@ public class Utils {
             return str;
         }
         return Character.toUpperCase(str.charAt(0)) + str.substring(1);
-    }
-
-    public static String formatarTempo(List<Double> tempos) {
-        StringBuilder resultado = new StringBuilder();
-
-        for (double tempoEmDecimal : tempos) {
-            int horas = (int) tempoEmDecimal;
-            int minutos = (int) ((tempoEmDecimal - horas) * 60);
-
-            if (horas > 0) {
-                resultado.append(horas).append(" hora");
-                if (horas > 1) {
-                    resultado.append("s");
-                }
-                resultado.append(" ");
-            }
-
-            if (minutos > 0 || (horas == 0 && minutos == 0)) {
-                resultado.append(minutos).append(" minuto");
-                if (minutos != 1) {
-                    resultado.append("s");
-                }
-            }
-
-            resultado.append("\n");
-        }
-
-        return resultado.toString();
     }
 
     public static String convertTime(int minutos) {
